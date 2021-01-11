e4cd9498d0ed5a7f4cea81605f0893c274a72699
George Kortsaridis <gkortsaridis@gmail.com>
2021-01-11 18:44:18 +0000

    Project setup

---
+++ src/Entities/AppRoutes.ts
@@ -0,0 +1,8 @@
+export const HOME = "/wellnesslab_web/dev/"
+export const SUBJECTS = "/wellnesslab_web/dev/subjects"
+export const SUBJECT_DETAILS = "/wellnesslab_web/dev/subjects/:subjectID"
+export const ACTIONS = "/wellnesslab_web/dev/actions"
+export const VIDEOS = "/wellnesslab_web/dev/videos"
+export const TEAM = "/wellnesslab_web/dev/team"
+
+export const ADMIN = "/wellnesslab_web/adminpanel"
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WellnessLabApp from "./Components/WellnessLabApp";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WellnessLabApp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
